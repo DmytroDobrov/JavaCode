af03c2c2da610bd60b5c886629c4bb35ff4ab8b6
DmytroDobrov2004 <ddobrov1@stu.vistula.edu.pl>
2024-08-10 17:11:46 +0200

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,42 @@
 import java.util.Scanner;
 
+interface voids{
+      void setName(String name);
+      void setSurname(String surname);
+      void setAge(int age);
+      void setLastWord(String lastWord);
 
-class Sentence{
+      void createSent();
+}
+class Sentence implements  voids{
     private String name;
     private String surname;
     private int age;
     private String lastWord;
+    @Override
     public void setName(String name) {
         this.name = name;
     }
+    @Override
     public void setSurname(String surname) {
         this.surname = surname;
     }
+    @Override
     public void setAge(int age) {
         this.age = age;
     }
+    @Override
     public void setLastWord(String lastWord) {
         this.lastWord = lastWord;
     }
+    @Override
     public void createSent(){
         System.out.println(name + " "+ surname + " " + age + " " + lastWord);
     }
 }
+class AlterSent extends Sentence{
 
+}
 
 public class Main {
     public static void main(String[] args) {
